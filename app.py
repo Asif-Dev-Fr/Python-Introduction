64c7c2069d9251c5b702caa881a84a39106ba079
Asif Kassam Ali <asif.kassamali@prioritis.com>
2022-08-01 18:06:39 +0200

    introduction started with some basic python

---
+++ app.py
@@ -0,0 +1,41 @@
+# Console Log 
+print("Hello world")
+
+# Variable
+first_name = "Asif"
+age = 33
+print('My name is ' + first_name)
+print(age)
+
+## Input (written in the terminal)
+# input_name = input("Name a country ? ")
+# print('Selected country : ' + input_name)
+
+# # Type of variable 
+# birthday = input("Enter your birthday ")
+# # parseInt
+# formatToInt = int(birthday)
+# age = 2022 - formatToInt
+# print("Your age is " + str(age))
+
+# float() = convert number like 10.5
+# bool() to convert to a boolean 
+
+# String in Python 
+course = "Python for Beginners"
+upperCaseString = course.upper()
+print(upperCaseString)
+# finds first index
+index = course.find("o")
+print(index)
+# replace 
+replace = course.replace("for", "4")
+print(replace)
+# include 
+include = "Python" in course
+print(include)
+
+# comparaison operator
+number = 3 == "3"
+print(number)
+# === does not exist ! here number is false in JS, it would have been true 
